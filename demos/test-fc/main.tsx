c1eab2cc1950318608b7cc04645068d68fb0b389
yxyHeart <56054482+yxyHeart@users.noreply.github.com>
2024-01-04 11:47:48 +0800

    feat: react-noop-renderer

--- demos/test-fc/main.tsx
+++ demos/test-fc/main.tsx
@@ -1,33 +1,38 @@
 import { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-noop-renderer';
 function App() {
-	const [num, updateNum] = useState(0);
-	useEffect(() => {
-		console.log('App mount');
-	}, []);
+	// const [num, updateNum] = useState(0);
+	// useEffect(() => {
+	// 	console.log('App mount');
+	// }, []);
 
-	useEffect(() => {
-		console.log('num change create', num);
-		return () => {
-			console.log('num change destroy', num);
-		};
-	}, [num]);
+	// useEffect(() => {
+	// 	console.log('num change create', num);
+	// 	return () => {
+	// 		console.log('num change destroy', num);
+	// 	};
+	// }, [num]);
 
 	return (
-		<div onClick={() => updateNum(num + 1)}>
-			{num === 0 ? <Child /> : 'noop'}
-		</div>
+		// <div onClick={() => updateNum(num + 1)}>
+		// 	{num === 0 ? <Child /> : 'noop'}
+		// </div>
+		<>
+			<div>1234</div>
+			<Child />
+		</>
 	);
 }
 
 function Child() {
-	useEffect(() => {
-		console.log('Child mount');
-		return () => console.log('Child unmount');
-	}, []);
+	// useEffect(() => {
+	// 	console.log('Child mount');
+	// 	return () => console.log('Child unmount');
+	// }, []);
 
 	return 'i am child';
 }
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<App />
-);
+const root = ReactDOM.createRoot();
+root.render(<App />);
+
+window.root = root;
